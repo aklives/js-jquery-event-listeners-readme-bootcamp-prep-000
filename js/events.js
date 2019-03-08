8056c48a5c08318a06d523ce3eb2339a72c52182
Adam Kearney <aklives@hotmail.com>
2019-03-08 14:48:38 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -12,8 +12,8 @@ function frameIt(){
 }
 
 function pressIt(){
-  $('form').on('keydown', function(){
-   if  ($('input:first').val() === "G"){
+  $(document).on('keydown', function(key){
+   if  ($(key.which === "G"){
      alert("You have pressed the G key");
    }
   
